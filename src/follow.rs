266ed32ac2ca4768bb99582679bd242766211a93
glowcouch <42631203+glowcouch@users.noreply.github.com>
2026-04-01 17:59:57 +1300

    feat: add vision sensor controller
    * feat: set up vision sensor tracking controller
    
    * refactor: convert vision tracking into angle-based
    
    * refactor: make vision tracking generic over control loop
    
    * docs: update docs for vision tracking
    
    * fix(vision): set up sma average
    
    * refactor(vision): use sma library
    
    * refactor(vision): make vision update falliable
    
    * feat(vision): write drive_towards_object
    
    * fix(vision): scale by angular errors

---
+++ src/follow.rs
@@ -0,0 +1,282 @@
+//! Alternative following algorithm to [`evian::motion::pursuit::PurePursuit`] that uses
+//! closed-loop feedback (such as pid) instead of arc-based turning.
+use core::{iter::FusedIterator, pin::Pin, task::Poll, time::Duration};
+use std::time::Instant;
+
+use evian::{
+    control::loops::{AngularPid, Feedback, Pid},
+    drivetrain::model::DrivetrainModel,
+    math::{Angle, Vec2},
+    motion::pursuit::Waypoint,
+    prelude::{Arcade, Drivetrain, Tolerances, TracksHeading, TracksPosition, TracksVelocity},
+};
+use vexide::time::{Sleep, sleep};
+
+/// A path following controller that uses closed-loop feedback for turning and driving.
+///
+/// This is suitable for dense paths (curves) and sparse paths (waypoints).
+#[derive(Debug, Clone)]
+pub struct PathFollow {
+    /// Linear feedback controller.
+    ///
+    /// This controls the robot's speed throughout the motion.
+    pub linear_controller: Pid,
+
+    /// Angular feedback controller.
+    ///
+    /// This controls the robot's heading throughout the motion.
+    pub angular_controller: AngularPid,
+
+    /// Lookahead radius.
+    ///
+    /// Points are considered "visited" when they are within this radius. A smaller radius will
+    /// result in higher accuracy, but can cause more oscilation.
+    pub lookahead: f64,
+
+    /// Throttle angle.
+    ///
+    /// When the angular error is greater than this angle the robot will not move forwards. Within
+    /// this angle, the throttle will be scaled linearly - max speed being when the error is
+    /// closest to 0.
+    pub throttle_angle: Angle,
+
+    /// The tolerances used for settling.
+    ///
+    /// The error is the distance to the last point.
+    /// The velocity is linear velocity.
+    pub tolerances: Tolerances,
+}
+
+impl PathFollow {
+    /// Follow a set of waypoints.
+    ///
+    /// Important note: the velocity of waypoints is ignored.
+    pub fn follow<
+        M: Arcade,
+        I: IntoIterator<Item = Waypoint>,
+        T: TracksPosition + TracksHeading + TracksVelocity,
+    >(
+        &self,
+        drivetrain: &mut Drivetrain<M, T>,
+        waypoints: I,
+    ) -> impl Future
+    where
+        <I as IntoIterator>::IntoIter: Clone + Unpin,
+    {
+        let Self {
+            linear_controller,
+            angular_controller,
+            lookahead,
+            throttle_angle,
+            tolerances,
+        } = self.clone();
+        let waypoints = waypoints.into_iter();
+        PathFollowFuture {
+            drivetrain,
+            // calculate the distance between all waypoints
+            path_distance: waypoints
+                .clone()
+                .map_windows(|window: &[Waypoint; 2]| {
+                    window[0].position.distance(window[1].position)
+                })
+                .sum(),
+            waypoints: waypoints.fuse(),
+            linear_controller,
+            angular_controller,
+            lookahead,
+            throttle_angle,
+            tolerances,
+            state: None,
+        }
+    }
+}
+
+/// State of [`PathFollowFuture`].
+struct State {
+    /// The current target waypoint.
+    ///
+    /// The robot will try to aim towards this position.
+    current: Waypoint,
+    /// The current sleep timer.
+    ///
+    /// The future uses this to sleep in between updates.
+    sleep: Sleep,
+
+    /// The previous time at which the controller was updated.
+    ///
+    /// This is used to calculate delta time.
+    prev_time: Instant,
+}
+
+/// Future returned by [`PathFollow::follow`].
+///
+/// The iterator has to implement [`FusedIterator`] because we use it to check if the path is finished.
+struct PathFollowFuture<
+    'a,
+    I: Iterator<Item = Waypoint> + FusedIterator,
+    M: DrivetrainModel + Arcade,
+    T: TracksPosition + TracksHeading + TracksVelocity,
+> {
+    /// The drivetrain being moved.
+    drivetrain: &'a mut Drivetrain<M, T>,
+
+    /// The set of waypoints that the robot is following.
+    ///
+    /// A waypoint is consumed each time that the [`State::current`] waypoint is reached.
+    waypoints: I,
+
+    /// The total distance between all the waypoints that are left.
+    ///
+    /// This must be updated whenever a waypoint is taken.
+    path_distance: f64,
+
+    /// Linear feedback controller.
+    ///
+    /// This is copied directly from [`PathFollow`].
+    linear_controller: Pid,
+
+    /// Angular feedback controller.
+    ///
+    /// This is copied directly from [`PathFollow`].
+    angular_controller: AngularPid,
+
+    /// The lookahead radius.
+    ///
+    /// This is copied directly from [`PathFollow`].
+    lookahead: f64,
+
+    /// Throttle angle.
+    ///
+    /// This is copied directly from [`PathFollow`].
+    throttle_angle: Angle,
+
+    /// The tolerances used for settling.
+    ///
+    /// This is copied directly from [`PathFollow`].
+    tolerances: Tolerances,
+
+    /// The state of the motion.
+    ///
+    /// This stores information for use within the future.
+    state: Option<State>,
+}
+
+impl<
+    I: Iterator<Item = Waypoint> + FusedIterator,
+    M: DrivetrainModel + Arcade,
+    T: TracksPosition + TracksHeading + TracksVelocity,
+> PathFollowFuture<'_, I, M, T>
+{
+    /// returns a heuristic for the total distance left in the motion
+    ///
+    /// takes the current tracking position
+    fn distance_heuristic(&self, position: Vec2<f64>) -> f64 {
+        // get the distance to the current waypoint (default to 0 if we don't have a current
+        // waypoint selected)
+        let distance_to_current = self
+            .state
+            .as_ref()
+            .map(|state| state.current.position.distance(position))
+            .unwrap_or_default();
+        distance_to_current + self.path_distance
+    }
+}
+
+impl<
+    I: Iterator<Item = Waypoint> + Unpin + FusedIterator,
+    M: DrivetrainModel + Arcade,
+    T: TracksPosition + TracksHeading + TracksVelocity,
+> Future for PathFollowFuture<'_, I, M, T>
+{
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut core::task::Context<'_>) -> Poll<Self::Output> {
+        let this = self.get_mut();
+
+        // get state or initialize it
+        let state = if let Some(state) = this.state.as_mut() {
+            state
+        } else {
+            // return `Poll::Ready` if there are no more waypoints
+            let Some(next_waypoint) = this.waypoints.next() else {
+                return Poll::Ready(());
+            };
+
+            // initialize state
+            this.state.insert(State {
+                current: next_waypoint,
+                sleep: sleep(Duration::from_millis(5)),
+                prev_time: Instant::now(),
+            })
+        };
+
+        // if the timer is not finished we go back to sleep
+        // I have confirmed that calling poll on a `vexide::time::Sleep` will in fact register the
+        // waker
+        if Pin::new(&mut state.sleep).poll(cx).is_pending() {
+            return Poll::Pending;
+        }
+
+        // start a new timer
+        state.sleep = sleep(Duration::from_millis(5));
+
+        // get tracking data
+        let position = this.drivetrain.tracking.position();
+        let heading = this.drivetrain.tracking.heading();
+
+        // find the next point that we have not reached
+        while state.current.position.distance(position) <= this.lookahead {
+            // find next waypoint (or check settling conditions if there are no more waypoints)
+            let Some(new_waypoint) = this.waypoints.next() else {
+                // if it is settled
+                if this.tolerances.check(
+                    position.distance(state.current.position),
+                    this.drivetrain.tracking.linear_velocity(),
+                ) {
+                    // we are done!
+                    drop(this.drivetrain.model.drive_arcade(0., 0.));
+                    return Poll::Ready(());
+                }
+                // if it is not settled, just exit the loop
+                break;
+            };
+            // update the distance that is left on the path
+            this.path_distance -= state.current.position.distance(new_waypoint.position);
+            // update current waypoint
+            state.current = new_waypoint;
+        }
+
+        // get delta time
+        let dt = state.prev_time.elapsed();
+        // update state
+        state.prev_time = Instant::now();
+
+        // update angular controller
+        let angular_setpoint = Angle::from_radians((state.current.position - position).angle());
+        let steer = this
+            .angular_controller
+            .update(heading, angular_setpoint, dt);
+
+        // save this for later because of lifetimes
+        let waypoint_limit = state.current.velocity;
+
+        // update linear controller
+        let throttle_scaling = (angular_setpoint - heading).wrapped_half().cos().abs(); // scale throttle based on the cos of the angular error
+        let linear_setpoint = this.distance_heuristic(position);
+        let throttle = this.linear_controller.update(-linear_setpoint, 0., dt) * throttle_scaling;
+
+        // drive the robot
+        drop(
+            this.drivetrain
+                .model
+                .drive_arcade(throttle.min(waypoint_limit), steer),
+        );
+
+        // wake ourselves once so that we can poll the timer (sleep)
+        //
+        // This doesn't cause a busy loop because we will return `Poll::Pending` once we poll the
+        // timer. It just makes sure that the timer has registered our waker
+        cx.waker().wake_by_ref();
+        Poll::Pending
+    }
+}
---
+++ src/vision.rs
@@ -0,0 +1,220 @@
+//! A controller that turns to face a vision sensor object.
+
+use std::{convert::Infallible, time::Instant};
+
+use anyhow::Context;
+use evian::{
+    control::loops::Feedback,
+    drivetrain::model::Arcade,
+    math::Angle,
+    prelude::{
+        Drivetrain, Tolerances, TracksForwardTravel, TracksHeading, TracksPosition, TracksVelocity,
+    },
+    tracking::Tracking,
+};
+use simple_moving_average::SMA;
+use simple_moving_average::SingleSumSMA;
+use vexide::{
+    math::Point2,
+    prelude::AiVisionSensor,
+    smart::{SmartDevice, ai_vision::AiVisionObject, vision},
+    time::sleep,
+};
+
+/// Wrapper around [`AiVisionObject::Color`].
+struct Color {
+    #[allow(clippy::missing_docs_in_private_items)]
+    id: u8,
+    #[allow(clippy::missing_docs_in_private_items)]
+    position: Point2<u16>,
+    #[allow(clippy::missing_docs_in_private_items)]
+    width: u16,
+    #[allow(clippy::missing_docs_in_private_items)]
+    height: u16,
+}
+
+/// Abstraction over vision sensor for better tracking.
+///
+/// `SMA` is the size of the simple moving average window
+struct VisionSensorAbstraction<'a, const SMA_WINDOW: usize> {
+    /// The vision sensor
+    sensor: &'a AiVisionSensor,
+    /// The id of the color object we are tracking.
+    object_id: u8,
+    /// SMA used to average the angle.
+    sma: SingleSumSMA<Angle, f64, SMA_WINDOW>,
+}
+
+impl<'a, const SMA_WINDOW: usize> VisionSensorAbstraction<'a, SMA_WINDOW> {
+    /// Update the vision sensor and get an angle value back.
+    fn update(&mut self) -> anyhow::Result<Angle> {
+        let objects = self.sensor.objects()?;
+
+        // find biggest color object
+        let biggest = objects
+            .iter()
+            .filter_map(|o| match *o {
+                AiVisionObject::Color {
+                    id,
+                    position,
+                    width,
+                    height,
+                } => Some(Color {
+                    id,
+                    position,
+                    width,
+                    height,
+                }),
+                _ => None,
+            })
+            .filter(|c| c.id == self.object_id)
+            .max_by_key(|c| c.width * c.height)
+            .context("no objects detected")?;
+
+        // calculate new error using sma
+        let error = color_angle(&biggest).wrapped_half();
+        self.sma.add_sample(error);
+
+        Ok(self.sma.get_average())
+    }
+
+    /// Create a new instance.
+    fn new(sensor: &'a AiVisionSensor, object_id: u8) -> Self {
+        Self {
+            sensor,
+            object_id,
+            sma: SingleSumSMA::from_zero(Angle::ZERO),
+        }
+    }
+}
+
+/// A controller that turns to face a vision sensor object.
+pub struct VisionTrack<
+    FA: Feedback<State = Angle, Signal = f64>,
+    FL: Feedback<State = f64, Signal = f64>,
+> {
+    /// The angular controller used for turning.
+    ///
+    /// Takes the robot's approximate heading relative to the object.
+    pub angular_controller: FA,
+
+    /// The linear controller used for driving.
+    ///
+    /// Takes the robot's distance from the target.
+    pub linear_controller: FL,
+
+    /// Tolerances used to determine when the robot has finished driving.
+    ///
+    /// Takes the robot's distance from the target.
+    pub linear_tolerances: Tolerances,
+}
+
+impl<
+    FA: Feedback<State = Angle, Signal = f64> + Clone,
+    FL: Feedback<State = f64, Signal = f64> + Clone,
+> VisionTrack<FA, FL>
+{
+    /// Point to face a vision sensor object.
+    ///
+    /// `object_id` is the id of the color to track (colors are the only ones supported currently).
+    ///
+    /// # Errors
+    ///
+    /// Will return an error if no object is detected by the vision sensor (or it is disconnected).
+    pub async fn turn_to_object<M: Arcade, T: Tracking>(
+        &self,
+        drivetrain: &mut Drivetrain<M, T>,
+        vision_sensor: &AiVisionSensor,
+        object_id: u8,
+    ) {
+        // state
+        let mut controller = self.angular_controller.clone();
+        let mut prev_time = Instant::now();
+        let mut sensor = VisionSensorAbstraction::<3>::new(vision_sensor, object_id);
+
+        loop {
+            sleep(AiVisionSensor::UPDATE_INTERVAL).await;
+
+            let error = sensor.update().unwrap_or_default();
+
+            // update controller
+            let dt = prev_time.elapsed();
+            prev_time = Instant::now();
+            let steer = controller.update(error, Angle::ZERO, dt);
+
+            // drive robot
+            drop(drivetrain.model.drive_arcade(0., steer));
+        }
+    }
+
+    /// Point to face a vision sensor object while driving some distance.
+    ///
+    /// `object_id` is the id of the color to track (colors are the only ones supported currently).
+    ///
+    /// # Errors
+    ///
+    /// Will return an error if no object is detected by the vision sensor (or it is disconnected).
+    pub async fn drive_towards_object<M: Arcade, T: TracksForwardTravel + TracksVelocity>(
+        &self,
+        drivetrain: &mut Drivetrain<M, T>,
+        vision_sensor: &AiVisionSensor,
+        object_id: u8,
+        distance: f64,
+    ) {
+        // state
+        let mut angular_controller = self.angular_controller.clone();
+        let mut linear_controller = self.linear_controller.clone();
+        let mut linear_tolerances = self.linear_tolerances;
+        let mut prev_time = Instant::now();
+        let mut sensor = VisionSensorAbstraction::<3>::new(vision_sensor, object_id);
+
+        // initial state
+        let target_travel = drivetrain.tracking.forward_travel() + distance;
+
+        loop {
+            sleep(AiVisionSensor::UPDATE_INTERVAL).await;
+
+            // get values
+            let angular_error = sensor.update().unwrap_or_default();
+            let dt = prev_time.elapsed();
+            prev_time = Instant::now();
+
+            // update angular controller
+            let steer = angular_controller
+                .update(angular_error, Angle::ZERO, dt)
+                .clamp(-1., 1.);
+
+            // update linear controller
+            let throttle = linear_controller
+                .update(drivetrain.tracking.forward_travel(), target_travel, dt)
+                .clamp(-1., 1.)
+                * angular_error.cos();
+            if linear_tolerances.check(throttle, drivetrain.tracking.linear_velocity()) {
+                break;
+            }
+
+            // drive robot
+            drop(drivetrain.model.drive_arcade(throttle, steer));
+        }
+    }
+}
+
+/// Returns the angle (from the center of the screen) of a color object.
+fn color_angle(object: &Color) -> Angle {
+    // find the center of that object (horizontally)
+    let center = f64::from(object.width) / 2. + f64::from(object.position.x);
+
+    // the center of the screen
+    let target = f64::from(AiVisionSensor::HORIZONTAL_RESOLUTION) / 2.;
+
+    // the distance in pixels
+    let distance = center - target;
+
+    // convert to angle
+    Angle::from_degrees(
+        // negate the answer so that negative values mean the object is to the right and positive
+        // values mean the object is to the left
+        -(f64::from(AiVisionSensor::HORIZONTAL_FOV) * distance)
+            / f64::from(AiVisionSensor::HORIZONTAL_RESOLUTION),
+    )
+}
