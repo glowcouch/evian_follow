be2f231d77366bd00428b121ccf330c2b88d5ddb
glowcouch <42631203+glowcouch@users.noreply.github.com>
2026-04-18 10:58:19 +1200

    feat(vision): change and clarify aabbfilter

--- src/vision/filters.rs
+++ src/vision/filters.rs
@@ -35,7 +35,7 @@ impl VisionFilter for Tuple {
     }
 }
 
-/// Filter out objects that are outside of the bounding box
+/// Filter out objects that do not intersect with the bounding box
 #[derive(Debug, Clone)]
 pub struct AABBFilter {
     /// The top left corner of box (in pixels).
@@ -47,10 +47,20 @@ pub struct AABBFilter {
 
 impl VisionFilter for AABBFilter {
     fn filter(&self, object: &Color) -> bool {
-        object.position.x > self.position.x
-            && object.position.y > self.position.y
-            && object.position.x + object.width < self.position.x + self.size.x
-            && object.position.y + object.height < self.position.y + self.size.y
+        let a_min = self.position;
+        let a_max = Point2 {
+            x: self.position.x + self.size.x,
+            y: self.position.y + self.size.y,
+        };
+
+        let b_min = object.position;
+        let b_max = Point2 {
+            x: object.position.x + object.width,
+            y: object.position.y + object.height,
+        };
+
+        // AABB-AABB intersection test
+        (a_min.x <= b_max.x && a_max.x >= b_min.x) && (a_min.y <= b_max.y && a_max.y >= b_min.y)
     }
 }
 
@@ -93,5 +103,70 @@ pub struct Not<T: VisionFilter>(pub T);
 impl<T: VisionFilter> VisionFilter for Not<T> {
     fn filter(&self, object: &Color) -> bool {
         !self.0.filter(object)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_aabb_filter_inside() {
+        let filter = AABBFilter {
+            position: Point2 { x: 10, y: 10 },
+            size: Point2 { x: 100, y: 100 },
+        };
+        let obj = Color {
+            position: Point2 { x: 20, y: 20 },
+            width: 10,
+            height: 10,
+            id: 0,
+        };
+        assert!(filter.filter(&obj));
+    }
+
+    #[test]
+    fn test_aabb_filter_outside_left() {
+        let filter = AABBFilter {
+            position: Point2 { x: 10, y: 10 },
+            size: Point2 { x: 100, y: 100 },
+        };
+        let obj = Color {
+            position: Point2 { x: 0, y: 20 },
+            width: 5,
+            height: 5,
+            id: 0,
+        };
+        assert!(!filter.filter(&obj));
+    }
+
+    #[test]
+    fn test_aabb_filter_outside_bottom_right() {
+        let filter = AABBFilter {
+            position: Point2 { x: 10, y: 10 },
+            size: Point2 { x: 100, y: 100 },
+        };
+        let obj = Color {
+            position: Point2 { x: 111, y: 111 },
+            width: 10,
+            height: 10,
+            id: 0,
+        };
+        assert!(!filter.filter(&obj));
+    }
+
+    #[test]
+    fn test_aabb_filter_on_edge() {
+        let filter = AABBFilter {
+            position: Point2 { x: 10, y: 10 },
+            size: Point2 { x: 100, y: 100 },
+        };
+        let obj = Color {
+            position: Point2 { x: 0, y: 0 },
+            width: 20,
+            height: 20,
+            id: 0,
+        };
+        assert!(filter.filter(&obj));
     }
 }
